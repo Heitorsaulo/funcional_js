8ecdc3759e08559cedaf162135282d7c04e1ce8c
Heitor Saulo <heitor-081@outlook.com>
2022-08-09 20:40:12 -0300

    att

--- Lista 4/Questão9_L4.js
+++ Lista 4/Questão9_L4.js
@@ -1,3 +1,9 @@
-const mmc = (n,m) =>{
-    
+const mdc = (n)=>(m) => {
+    if (n>m) return mdc(m)(n)
+    else if (n==0) return m
+    else return mdc(n)(m-n)
 }
+
+const mmc = (n) => (m) => m*n/mdc(n)(m)
+
+console.log(mmc(8)(16))
